6043ef2edeb6767ad9f46e46959ef1ae346928fa
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-04-07 00:51:33 +0500

    updated day24 code

---
+++ day-24/README.md
@@ -0,0 +1,2 @@
+# Day-24-coding-Challenge
+In this repository,3 questions are included. Question # 70, 71 And 72
---
+++ day-24/main.js
@@ -0,0 +1,35 @@
+// Day 24 Challenge: Start Coding! 
+// Question 70: Understanding let in Loops: Write a function that uses a loop with the let keyword to print numbers from 1 to 5. Explain how the let keyword affects the visibility of the loop variable.
+//Three ways of defining 
+console.log(`\n\t******Day 24 Coding Challenge****** \n\n\tQuestion 70: Understanding let in Loops\n`);
+function numLoop(...numArray) {
+    for (let i = 0; i < numArray.length; i++) {
+        console.log(`Numbers in a given array are: ${numArray[i]}`);
+    }
+}
+numLoop(1, 2, 3, 4, 5);
+function numwithlet() {
+    for (let a = 1; a <= 5; a++) {
+        console.log(`number are:`, a);
+    }
+}
+numwithlet();
+// Question 71: Compare let and const: Create two examples where let allows reassignment but const does not. Try to reassign a const-declared variable and catch the error.
+console.log(`\n\tQuestion 71: Compare let and const:\n`);
+let personName = "Samreen Saif";
+const personRollnumber = 65146;
+console.log(`Person Name: ${personName} \nRoll Number: ${personRollnumber}`);
+personName = "Firzah Fatima"; // can reassign let variable
+//personRollnumber = 34523; // cannot reassign const variable
+console.log(`Person Name: ${personName} \nRoll Number: ${personRollnumber}`);
+// Question 72: Block Scope with let and const: Demonstrate block scope by creating a code block with {} that uses both let and const. Show how variables declared inside the block are not accessible outside of it.
+// Explain & TIP: Block scope, created by {} in JavaScript, confines let and const variables to that block, enhancing code organization and preventing accidental access from outside the block.
+console.log(`\n\tQuestion 72: Block Scope with let and const:\n`);
+function letConst() {
+    let personName1 = "Syed Musaab Saif"; //variable inside a block
+    const personRollnumber1 = 349856;
+    console.log(`\nPerson Name: ${personName1} \nRoll Number: ${personRollnumber1}`);
+}
+letConst();
+export {};
+//console.log(personName1,personRollnumber1)   // variables inside a block are not accessible outside the block
