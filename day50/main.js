98d675583d467e06593934546de2751f1b342df8
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-04-20 13:27:15 +0500

    updated day50 code

---
+++ day50/main.js
@@ -0,0 +1,35 @@
+// Day 50 Challenge: Start Coding! 🚀
+// Question 148: Demonstrate the use of the setTimeout() function to execute code after a delay.
+console.log(`\n\n\tsetTimeOut()`);
+setTimeout(() => { console.log(`\nHello Samreen`); }, 1000);
+export {};
+/*
+
+// Question 149: Explain the concept of the event loop in JavaScript with an example.
+
+console.log(`\n\tEvent Loop By setTimeOut()`)
+
+console.log(`befor delay`)
+
+setTimeout(() =>
+    {
+        console.log(`Pakistan Zindabad`)},3000 )
+
+console.log(`after delay`);
+
+/*
+// Question 150: Describe how asynchronous callbacks differ from synchronous code execution.
+
+// Synchronous example
+console.log("Before synchronous ");
+// Simulate a synchronous blocking operation
+for (let i = 0; i < 1e10; i++) {} // A long loop
+console.log("After synchronous ");
+
+// Asynchronous example
+console.log("\n\nBefore asynchronous ");
+setTimeout(() => {
+  console.log("Asynchronous operation ");
+}, 10000);
+console.log("After asynchronous ");
+// Demonstrates how the rest of the code continues to execute without waiting for the asynchronous operation. */ 
