9a85bee290551677e4e0ba76bacb3347ead69a6c
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-05-30 13:54:50 +0500

    change in syntax

--- 1-45assignments/asgn_23/asgn23Conditionaltest.ts
+++ 1-45assignments/asgn_23/asgn23Conditionaltest.ts
@@ -1,46 +1,48 @@
-/*Conditional Tests: Write a series of conditional tests. Print a statement
-describing each test and your prediction for the results of each test. Your code
-should look something like this:
-let car = 'subaru';
- conditional test*/
-//test #1
+// /*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
 
 let car: string = "subaru";
  
-  //TEST #1
-console.log ("is car==`subaru`? i predict True");
-console.log(car =="subaru");
-//Test#2
-console.log ("is car===`subaru`? i predict True");
-console.log(car ==="subaru");
-//Test #3
-console.log ("is car ==`Honda`? i predict False");
-console.log(car =="Honda");
-//Test #4
-console.log ("is car !=`Honda`? i predict True");
-console.log(car !="Honda");
-//Test#5
-console.log ("is car !==`ford`? i predict true");
-console.log(car !=="ford");
-// Test #6
-console.log ("is car.uppercase ==`SUBARU`? i predict false");
-console.log(car.toUpperCase() =="subaru");
 
-// Test #7
-console.log ("is car ==`SUBARU`? i predict false");
-console.log(car =="SUBARU");
+car = "toyota"
 
-// Test #8
-console.log ("is car ===`SUBARU`? i predict false");
-console.log(car ==="SUBARU");
+console.log(car == "subaru" ? "its Subaru" : "its not Subaru"); // by using ternary operator
 
-// Test #9
-console.log ("is car ===`subaru`? i predict true");
-console.log(car ==="subaru");
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
 
-// Test #10
-console.log ("is car ==`plane`? i predict false");
-console.log(car =="plane");
+
+if(car === "subaru")
+    {  console.log("\nIts Subaru")}
+else{console.log("\nIts not Subaru");}
+
+
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+
+let x = 40
+let y = 30
+
+//falsey statement
+
+console.log(`Test1: x is equal to 5`, x===5);
+console.log(`Test2: x is less than 5`, x<5);
+console.log(`Test3: x is greater than multiple of 5`, x===5*5);
+console.log(`Test4: x is greater than multiple of 10`, x>10*10);
+console.log(`Test5: x is less than y`, x<y);
+
+//truthy statement
+
+console.log(`\n\nTest1: x is not equal to 5`, x!==5);
+console.log(`Test2: x is greater than 5`, x>5);
+console.log(`Test3: x is greater than multiple of 5`, x>5*5);
+console.log(`Test4: x is less than multiple of 10`, x<10*10);
+console.log(`Test5: x is greater than y`, x>y);
 
 
 
